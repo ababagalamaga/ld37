e9fd638d4d5e029fc699a7a74ac560c2177a0d1e
Mykyta Smaha <smaganik@gmail.com>
2016-12-11 20:16:04 +0200

    Blur setting added.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,20 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.ImageEffects;
 
 public class PlayerController : MonoBehaviour {
 
     private PlayerMovement _playerMovement;
     private CameraController _cameraController;
+
+    private DepthOfField _blur;
+    private GameObject _bloom;
+
+    private float _currentBlurAperture;
+    private float _targetBlurAperture;
+    private float _blurApertureDuration;
+    private float _blurAperturePassed;
+    private bool _blurApertureEnabled;
     // Use this for initialization
     void Awake () {
 	    _playerMovement = GetComponent<PlayerMovement>();
         _cameraController = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraController>();
+
+        _blur = _cameraController.GetComponent<DepthOfField>();
+        _currentBlurAperture = 0.0f;
+        _targetBlurAperture = 0.0f;
+        _blurApertureDuration = 0.0f;
+        _blurAperturePassed = 0.0f;
+        _blurApertureEnabled = false;
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
+	    if (_blurAperturePassed < _blurApertureDuration && _blurApertureDuration > 0.0f) {
+	        _blurAperturePassed += Time.deltaTime;
+	        var passedNorm = _blurAperturePassed / _blurApertureDuration;
+	        _blur.aperture = _currentBlurAperture * (1 - passedNorm) + _targetBlurAperture * passedNorm;
+	    } else {
+	        _blur.aperture = _targetBlurAperture;
+            _blur.enabled = _blurApertureEnabled;
+	    }
 	}
 
     public void ApplySettings(Room room) {
@@ -26,5 +50,14 @@ public class PlayerController : MonoBehaviour {
         _cameraController.HeadBobAmount = room.PlayerHeadBobAmount;
         _cameraController.HeadBobDuration = room.PlayerHeadBobDuration;
         _cameraController.HeadBobError = room.PlayerHeadBobError;
+
+        _blurApertureEnabled = room.PlayerBlurEnabled;
+        if (_blurApertureEnabled) {
+            _blur.enabled = _blurApertureEnabled;
+        }
+        _blurApertureDuration = room.PlayerBlurDuration;
+        _currentBlurAperture = _blur.aperture;
+        _targetBlurAperture = room.PlayerBlurAperture;
+        _blurAperturePassed = 0.0f;
     }
 }
