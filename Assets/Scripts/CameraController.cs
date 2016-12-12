3535cf9fd14548d756f79e7bb885c9199477e592
Mykyta Smaha <smaganik@gmail.com>
2016-12-12 16:55:51 +0200

    Lava prefab fixed.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -48,7 +48,7 @@ public class CameraController : MonoBehaviour
     void Update() {
         _currentHeight = Mathf.Lerp(_currentHeight, _targetHeight, Time.deltaTime * 10.0f);
 
-        if (_playerSpeed > HeadBobMinSpeed && HeadBobDuration > 0.0f) {
+        if (_playerSpeed > HeadBobMinSpeed && HeadBobDuration > 0.0f && HeadBobRotationDuration > 0.0f) {
             _headBobPassed += Time.deltaTime;
             if (_headBobPassed > HeadBobDuration) {
                 _headBobPassed -= HeadBobDuration;
