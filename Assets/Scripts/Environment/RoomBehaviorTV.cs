c3e6eda6540dfb9cf0dc3fdcb0c62e19bcbb943f
Aleksandr <alex.legend.ignatenko@gmail.com>
2016-12-12 15:38:56 +0200

    tv room behavior added

--- Assets/Scripts/Environment/RoomBehaviorTV.cs
+++ Assets/Scripts/Environment/RoomBehaviorTV.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class RoomBehaviorTV : MonoBehaviour
 {
 
-    public bool ObjectiveSucced = false;
+    public bool ObjectiveSucced = true;
     public float Period = 2.0f;
 
     private float _timeLeft = 0f;
-    private bool _isWatching = false;
+    private bool _isWatching = true;
     private Room _room;
     private GameObject _player;
     private GameObject _tv;
@@ -24,11 +24,38 @@ public class RoomBehaviorTV : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {   
-        if (ObjectiveSucced)
-        {
-            _room.ObjectiveSucced = true;
-            Destroy(this);
+    {
+         _timeLeft -= Time.deltaTime;
+
+        if (_timeLeft < 0) {
+            _isWatching = !_isWatching;
+            Material mat = _tv.GetComponent<Renderer>().materials[1];
+            if (_isWatching) {
+                mat.SetColor("Albedo", Color.white);
+            } else {
+                mat.SetColor("Albedo", Color.black);
+            }
+            _timeLeft = Period;
+        }
+        if (_isWatching) {
+            Vector3 raycast_direction = _player.transform.position - _tv.transform.position;
+            RaycastHit hit;
+
+            if (Physics.Raycast(_tv.transform.position, raycast_direction, out hit)) {
+                if (hit.collider.gameObject.tag == "Player") {
+                    print("I SEE YOU");
+                    ObjectiveSucced = false;
+                }
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "Player") {
+            if (ObjectiveSucced) {
+                _room.ObjectiveSucced = true;
+                Destroy(this);
+            }
         }
     }
 }
