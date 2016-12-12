a0bd9e56b510e66b7bf9655dbb3bad36045f783d
Mykyta Smaha <smaganik@gmail.com>
2016-12-12 21:53:54 +0200

    Drunk movement added.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -18,14 +18,21 @@ public class CameraController : MonoBehaviour
     public float HeadBobRotationPhase;
     public float HeadBobRotationPhaseMult;
     public float HeadBobRotationLerp;
+    public float HeadBobForwardAmount;
+    public float HeadBobForwardPhase;
+    public float HeadBobForwardPhaseMult;
+    public float HeadBobForwardLerp;
+    public float HeadBobForwardDuration;
 
     private GameObject _playerController;
     private float _targetHeight;
     private float _currentHeight;
     private Vector3 _headBobOffset;
     private Quaternion _headBobRotation;
+    private Quaternion _headBobForward;
     private float _headBobPassed;
     private float _headBobRotationPassed;
+    private float _headBobForwardPassed;
     private float _playerSpeed;
     private Quaternion _realRotation;
 
@@ -34,6 +41,7 @@ public class CameraController : MonoBehaviour
 	    _playerController = GameObject.FindGameObjectWithTag("Player");
         _headBobOffset = new Vector3(0, 0, 0);
         _headBobRotation = Quaternion.identity;
+        _headBobForward = Quaternion.identity;
         _realRotation = transform.rotation;
     }
 
@@ -59,6 +67,20 @@ public class CameraController : MonoBehaviour
                 _headBobRotationPassed -= HeadBobRotationDuration;
             }
 
+            _headBobForwardPassed += Time.deltaTime;
+            if (_headBobForwardPassed > HeadBobForwardDuration) {
+                _headBobForwardPassed -= HeadBobForwardDuration;
+            }
+
+            if (HeadBobForwardDuration > 0.0f) {
+                var passedForwardNorm = _headBobForwardPassed / HeadBobForwardDuration;
+                passedForwardNorm = Mathf.Pow(passedForwardNorm, HeadBobError > 0.0f ? HeadBobError : 1.0f);
+                var forwardDeltaX = Mathf.Sin(((passedForwardNorm * Mathf.PI * 2.0f) + HeadBobForwardPhase) * HeadBobForwardPhaseMult) * HeadBobForwardAmount;
+                var forwardDeltaY = Mathf.Cos(((passedForwardNorm * Mathf.PI * 2.0f) + HeadBobForwardPhase + Mathf.PI * 1.0f) * HeadBobForwardPhaseMult) * HeadBobForwardAmount;
+
+                _headBobForward = Quaternion.Lerp(_headBobForward, Quaternion.LookRotation(new Vector3(forwardDeltaX, forwardDeltaY, HeadBobForwardAmount).normalized), Time.deltaTime * HeadBobForwardLerp);
+            }
+
             var passedNorm = _headBobPassed / HeadBobDuration;
             var passedRotationNorm = _headBobRotationPassed / HeadBobRotationDuration;
             passedNorm = Mathf.Pow(passedNorm, HeadBobError > 0.0f ? HeadBobError : 1.0f);
@@ -81,6 +103,7 @@ public class CameraController : MonoBehaviour
             _headBobRotationPassed = 0;
             _headBobOffset = Vector3.Lerp(_headBobOffset, new Vector3(0, 0, 0), Time.deltaTime * HeadBobLerp);
             _headBobRotation = Quaternion.Lerp(_headBobRotation, Quaternion.identity, Time.deltaTime * HeadBobRotationLerp);
+            _headBobForward = Quaternion.Lerp(_headBobForward, Quaternion.identity, Time.deltaTime * HeadBobRotationLerp);
             var speedMult = 1.0f;
             if (HeadBobAmount > 0.0f) {
                 speedMult = (_headBobOffset.y + HeadBobAmount) * 0.5f / HeadBobAmount;
@@ -95,7 +118,7 @@ public class CameraController : MonoBehaviour
             Time.deltaTime * LookLerpCoeff);
 	    _realRotation = Quaternion.LookRotation(_realRotation * Vector3.forward);
 
-        transform.rotation =  _realRotation * _headBobRotation;
+	    transform.rotation = _realRotation * _headBobRotation * _headBobForward;
 
         transform.position = Vector3.Lerp(_playerController.transform.position + Vector3.up * _currentHeight + _headBobOffset, transform.position, Time.deltaTime * MoveLerpCoeff);
 	}
