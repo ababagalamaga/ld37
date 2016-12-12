f4afd444d9f2652619b1b4d23182c78927ace23f
Aleksandr <alex.legend.ignatenko@gmail.com>
2016-12-12 14:01:21 +0200

    script for tv room

---
+++ Assets/Scripts/Environment/RoomBehaviorTV.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomBehaviorTV : MonoBehaviour
+{
+
+    public bool ObjectiveSucced = false;
+    public float Period = 2.0f;
+
+    private float _timeLeft = 0f;
+    private bool _isWatching = false;
+    private Room _room;
+    private GameObject _player;
+    private GameObject _tv;
+
+    // Use this for initialization
+    void Start()
+    {
+        _tv = GameObject.Find("TV");
+        _room = GetComponent<Room>();
+        _player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        if (ObjectiveSucced)
+        {
+            _room.ObjectiveSucced = true;
+            Destroy(this);
+        }
+    }
+}
