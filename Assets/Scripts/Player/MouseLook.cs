e1901499a098478e2ebd16453fb57f83a9583f60
Mykyta Smaha <smaganik@gmail.com>
2016-12-12 20:36:38 +0200

    Look Limitations added.

--- Assets/Scripts/Player/MouseLook.cs
+++ Assets/Scripts/Player/MouseLook.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class MouseLook : MonoBehaviour {
 
     public float Sensitivity;
+    public float MaxUp;
+    public float MinDown;
 
     private Transform _transform;
     private bool _rotationEnabled;
@@ -21,7 +23,17 @@ public class MouseLook : MonoBehaviour {
 	    if (_rotationEnabled) {
             _transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X") * Sensitivity, 0), Space.World);
             _transform.Rotate(new Vector3(-Input.GetAxis("Mouse Y") * Sensitivity, 0, 0));
-        }
+
+	        var rotation = _transform.rotation;
+	        var angles = rotation.eulerAngles;
+	        if (angles.x > 0.0f && angles.x < 90.0f) {
+	            angles.x = Mathf.Clamp(angles.x, 0.0f, MinDown);
+            } else if (angles.x > 270.0f && angles.x < 360.0f) {
+	            angles.x = Mathf.Clamp(angles.x, MaxUp, 360.0f);
+            }
+
+            _transform.rotation = Quaternion.Euler(angles);
+	    }
 	}
 
     public Quaternion Rotation() {
