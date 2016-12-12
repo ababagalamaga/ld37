e738be68a4d52f0b8b6841bbe48e7f28e49e4a1c
Aleksandr <alex.legend.ignatenko@gmail.com>
2016-12-12 17:49:43 +0200

    added light for tv

--- Assets/Scripts/Environment/RoomBehaviorTV.cs
+++ Assets/Scripts/Environment/RoomBehaviorTV.cs
@@ -13,11 +13,14 @@ public class RoomBehaviorTV : MonoBehaviour
     private Room _room;
     private GameObject _player;
     private GameObject _tv;
+    private GameObject _tvLight;
 
     // Use this for initialization
     void Start()
     {
+        print("started");
         _tv = GameObject.Find("TV");
+        _tvLight = GameObject.Find("TVLight");
         _room = GetComponent<Room>();
         _player = GameObject.FindGameObjectWithTag("Player");
     }
@@ -25,22 +28,32 @@ public class RoomBehaviorTV : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-         _timeLeft -= Time.deltaTime;
-
+        _timeLeft -= Time.deltaTime;
         if (_timeLeft < 0) {
+            print("swap");
             _isWatching = !_isWatching;
-            Material mat = _tv.GetComponent<Renderer>().materials[1];
-            if (_isWatching) {
-                mat.SetColor("Albedo", Color.white);
-            } else {
-                mat.SetColor("Albedo", Color.black);
-            }
             _timeLeft = Period;
+            if (_isWatching) {
+                _tv.GetComponent<Renderer>().materials[0].color = Color.white;
+                _tv.GetComponent<Renderer>().materials[1].color = Color.white;
+                _tv.GetComponent<Renderer>().materials[2].color = Color.white;
+                _tvLight.GetComponent<Light>().enabled = _isWatching;
+                //mat[0].color = Color.white;
+                //mat[0].SetColor("Color", Color.white);
+            } else {
+                _tv.GetComponent<Renderer>().materials[0].color = Color.black;
+                _tv.GetComponent<Renderer>().materials[1].color = Color.black;
+                _tv.GetComponent<Renderer>().materials[2].color = Color.black;
+                _tvLight.GetComponent<Light>().enabled = _isWatching;
+                //mat[0].color = Color.black;
+                //0[mat].SetColor("Color", Color.black);
+            }
         }
         if (_isWatching) {
             Vector3 raycast_direction = _player.transform.position - _tv.transform.position;
             RaycastHit hit;
 
+            _tvLight.GetComponent<Transform>().rotation = Quaternion.LookRotation(raycast_direction);
             if (Physics.Raycast(_tv.transform.position, raycast_direction, out hit)) {
                 if (hit.collider.gameObject.tag == "Player") {
                     print("I SEE YOU");
@@ -50,9 +63,10 @@ public class RoomBehaviorTV : MonoBehaviour
         }
     }
 
-    void OnTriggerEnter(Collider other) {
+    void OnTriggerExit(Collider other) {
         if (other.gameObject.tag == "Player") {
             if (ObjectiveSucced) {
+                print("destroy");
                 _room.ObjectiveSucced = true;
                 Destroy(this);
             }
