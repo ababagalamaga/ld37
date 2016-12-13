68411e984bbf79c697a68008e6baae471bc78a47
Mykyta Smaha <smaganik@gmail.com>
2016-12-13 04:39:28 +0200

    Game end logics.

--- Assets/Scripts/Environment/RoomBehavior6.cs
+++ Assets/Scripts/Environment/RoomBehavior6.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class RoomBehavior6 : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public GameObject Mom;
+    private Room _room;
+    private GameObject _player;
+
+    // Use this for initialization
+    void Start () {
+        _room = GetComponent<Room>();
+        _player = GameObject.FindGameObjectWithTag("Player");
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+	    if (_room.PlayerInRoom()) {
+	        if (Mom.GetComponent<MomSuccessNotifier>() != null && Mom.GetComponent<MomSuccessNotifier>().Triggered) {
+	            _room.ObjectiveSucced = true;
+                Time.timeScale = 0;
+            }
+	    }
 	}
 }
