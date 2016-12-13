9c463439176839861cfb53087eb04a1ba7beae40
Mykyta Smaha <smaganik@gmail.com>
2016-12-13 05:05:36 +0200

    Final commit.

--- Assets/Scripts/Environment/RoomBehavior6.cs
+++ Assets/Scripts/Environment/RoomBehavior6.cs
@@ -6,12 +6,10 @@ public class RoomBehavior6 : MonoBehaviour {
 
     public GameObject Mom;
     private Room _room;
-    private GameObject _player;
 
     // Use this for initialization
     void Start () {
         _room = GetComponent<Room>();
-        _player = GameObject.FindGameObjectWithTag("Player");
     }
 	
 	// Update is called once per frame
